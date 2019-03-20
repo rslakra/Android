0383248318f22ad63de0fc2f2e9d5a22815a44d7
Rohtash Singh Lakra <rohtash.singh@gmail.com>
2019-03-20 09:09:29 -0700

    Added Sender Sample App.

---
+++ BroadcastReceiver/Sender/app/src/main/java/com/rslakra/sender/MainActivity.java
@@ -0,0 +1,22 @@
+package com.rslakra.sender;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendBroadcastMessage(View view) {
+        Intent intent = new Intent("receiver.action.name");
+        sendBroadcast(intent);
+    }
+}
